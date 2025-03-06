aaec4686cb23d9146dbac14bcfa797e59f285421
pangzhen82 <pangzhen82@gmail.com>
2025-03-06 14:36:32 -0700

    ChatServer(console app)

---
+++ ChatServer/Client.cs
@@ -0,0 +1,61 @@
+﻿using ChatServer.Net.IO;
+using System.Net.Sockets;
+
+namespace ChatServer
+{
+    internal class Client
+    {
+        public string Username  { get; set; }
+        public Guid UID { get; set; }
+        public TcpClient ClientSocket { get; set; }
+        PacketReader _packetReader;
+        public Client(TcpClient client)
+        {
+            ClientSocket = client;
+            UID = Guid.NewGuid();
+            _packetReader = new PacketReader(ClientSocket.GetStream());
+            
+            var opcode = _packetReader.ReadByte();
+            Username = _packetReader.ReadMessage();
+
+            Console.WriteLine($"[{DateTime.Now}]: Client has connected with the username: {Username}");
+            Program.BroadcastMessage($"{Username} is now connected...", "[System]");
+
+            Task.Run(() => Process());
+        }
+        public void Process()
+        {
+            while (true)
+            {
+                try
+                {
+                    var opcode = _packetReader.ReadByte();
+                    switch (opcode)
+                    {
+                        case 5:
+                            var msg = _packetReader.ReadMessage();
+                            var name = _packetReader.ReadMessage();
+                            Program.BroadcastMessage(msg, Username);
+                            break;
+                        case 6:
+                            var whispermsg = _packetReader.ReadMessage();
+                            var whisperName = _packetReader.ReadMessage();
+                            var whisperReceiverName = _packetReader.ReadMessage();
+                            Program.SendMessageTo(whispermsg, whisperName, whisperReceiverName);
+                            break;
+                        default:
+                            Console.WriteLine($"Default opcode({opcode})............"); ////////// ---------> Test line
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"[{DateTime.Now}] {Username}: Disconnected!");
+                    Program.BroadcastDisconnect(UID.ToString());
+                    ClientSocket.Close();
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ ChatServer/Net/IO/PacketBuilder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatServer.Net.IO
+{
+    internal class PacketBuilder : IDisposable
+    {
+        MemoryStream _ms;
+        public PacketBuilder()
+        {
+            _ms = new MemoryStream();
+        }
+        public void WriteOpCode(byte opcode)
+        {
+            _ms.WriteByte(opcode);
+        }
+        public void WriteMessage(string msg)
+        {
+            byte[] messageBytes = Encoding.UTF8.GetBytes(msg);
+            _ms.Write(BitConverter.GetBytes(messageBytes.Length));
+            _ms.Write(messageBytes);
+        }
+        public byte[] GetPacketBytes()
+        {
+            return _ms.ToArray();
+        }
+        public void Dispose()
+        {
+            _ms?.Dispose();
+        }
+    }
+}
